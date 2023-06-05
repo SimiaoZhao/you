f07816ccc77c879a41b71498b1205b2ff87ffc0b
SimiaoZhao <Zhaosimiao1@gmail.com>
2023-06-05 17:57:45 +0100

    simiao version

---
+++ api/index.js
@@ -0,0 +1,80 @@
+const { Configuration, OpenAIApi } = require("openai");
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const operandClient = require("@operandinc/sdk").operandClient;
+const indexIDHeaderKey = require("@operandinc/sdk").indexIDHeaderKey;
+const ObjectService = require("@operandinc/sdk").ObjectService;
+
+// Open AI Configuration
+const configuration = new Configuration({
+  organization: "org-MgzdcfxN4df81USJrxl9cH4M",
+  apiKey: "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",
+});
+
+const openai = new OpenAIApi(configuration);
+
+// Express Configuration
+const app = express();
+const port = 3080;
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use(require("morgan")("dev"));
+
+// Routing
+
+// Primary Open AI Route
+app.post("/", async (req, res) => {
+  const { message } = req.body;
+
+  const runIndex = async () => {
+    const operand = operandClient(
+      ObjectService,
+      process.env.OPERAND_KEY,
+      "https://api.operand.ai",
+      {
+        [indexIDHeaderKey]: process.env.OPERAND_INDEX_KEY,
+      }
+    );
+
+    try {
+      const results = await operand.searchWithin({
+        query: `${message}`,
+        limit: 5,
+      });
+
+      if (results) {
+        return results.matches.map((m) => `- ${m.content}`).join("\n");
+      } else {
+        return "";
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  let operandSearch = await runIndex(message);
+
+  const basePromptPrefix = `This is a conversation between simiao zhao and himself. He is recently starting a startup with his friends`;
+  // const basePromptPrefix = `This is a conversation between sim and himself.\nRelevant information that Siraj knows:\n${operandSearch}`;
+
+  const response = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: `${basePromptPrefix}\n\nsimiao zhao:${message}\n\nsimiao:`,
+    max_tokens: 256,
+    temperature: 0.7,
+  });
+  res.json({
+    message: response.data.choices[0].text,
+  });
+});
+
+// Get Models Route
+
+// Start the server
+app.listen(port, () => {
+  console.log(`server running`);
+});
+
+module.exports = app;
---
+++ client/src/OpenAISVGLogo.js
@@ -0,0 +1,14 @@
+// OpenAI SVG Logo
+
+const OpenAISVGLogo = () => (
+  <div className="w-1">
+    <div style={{ width: "50px", height: "50px" }}>
+      <img
+        src="./simiao.jpeg"
+        style={{ width: "100%", height: "100%", borderRadius: "50%" }}
+      ></img>
+    </div>
+  </div>
+);
+
+export default OpenAISVGLogo;
